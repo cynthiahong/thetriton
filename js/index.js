f947e788627441f2f2057de6c44000d7fe5bed2a
Cynthia Hong <cynthiahong@Cynthias-MacBook-Pro-2.local>
2017-02-21 22:08:59 -0800

    testing

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+/* sticky navigation bar */
+$(document).ready(function () {
+  var $nav = $('#navigation'),
+      posTop = $nav.position().top;
+  $(window).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > posTop) { $nav.addClass('fixed'); }
+    else { $nav.removeClass('fixed'); }
+  });
+});
