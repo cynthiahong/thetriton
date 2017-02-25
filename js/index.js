3cb9ac5fb75ac5ae2efb9dabdee9cb5d7846081f
Cynthia Hong <cynthiahong@Cynthias-MacBook-Pro-2.local>
2017-02-25 11:48:40 -0800

    test

--- js/index.js
+++ js/index.js
@@ -59,3 +59,24 @@ window.onload = function() {
     GetClock();
     setInterval(GetClock, 1000);
 }
+
+
+
+// v3.1.0
+//Docs at http://simpleweatherjs.com
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'La Jolla, CA',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+;
+
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
