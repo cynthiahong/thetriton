f9b8e6419ba9186327f29aa7de2e4f5d212c2037
Jessica Kwen <hayoungkwen@gmail.com>
2017-03-14 15:55:11 -0700

    Editted CSS

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     });
 });
 
-/* slideshow on homepage */
+/* slideshow on homepage
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -43,17 +43,49 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block"; 
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000);
+} */
+
+
+
+/* var slideIndex = 1;
+showSlides(slideIndex); */
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+    console.log(slideIndex);
+}
+
 
 /* makes the Triton logo appear when scrolled down on desktop */
 $(document).scroll(function () {
